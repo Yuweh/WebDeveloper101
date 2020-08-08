b0f4adeba2ba954a5bbf3a18fcc66137df4225d3
Jay <Yuweh@users.noreply.github.com>
2020-08-08 19:51:47 +0800

    Update Basics.js

--- Javascript/Basics.js
+++ Javascript/Basics.js
@@ -1,1 +1,42 @@
 # Javascript
+
+
+alert("hello");
+typeof(123); //number
+
+var yourName = prompt("what is your name");
+
+#Data Types
+String -> "Hello"
+Number -> 123
+Boolean -> true || false
+
+// Introduction to Javascript -> JS was made in 10 days WTF hahaha
+//Javascript Alerts - Adding Behaviour to Websites -> alert("hello"); thats it
+//Data Types - Variables, Numbers, and Booleans
+//Javascript Variables - var is keyword
+//Javascript Variables Exercise Start
+//Naming and Naming Conventions for Javascript Variables - use camel casing and use meaningful names and not var as name, JS doesn;t allow it
+//String Concatenation -> alert(intro + " to " + subject);
+//String Lengths and Retrieving the Number of Characters - variableName.length
+//Challenge: Changing Casing in Text -> variableName.toUpperCase()
+//Challenge: Changing String Casing Solution
+
+Basic Arithmetic and the Modulo Operator in Javascript
+Increment and Decrement Expressions
+Javascript Numbers Quiz
+Functions Part 1: Creating and Calling Functions
+Functions Part 1 Challenge - The Karel Robot
+The Karel Chess Board Solution
+A Quick Note About the Next Lesson
+Functions Part 2: Parameters and Arguments
+Life in Weeks Coding Exercise
+Life in Weeks Solution
+Functions Part 3: Outputs & Return Values
+Challenge: Create a BMI Calculator
+BMI Calculator Challenge
+Challenge: BMI Calculator Solution
+JavaScript Functions Quiz
+Tip from Angela - Set Your Expectations
+Feedback Time!
+
