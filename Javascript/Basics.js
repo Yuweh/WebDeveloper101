27233b3cdb617f554a51979330f628e68e9a1e15
Jay <Yuweh@users.noreply.github.com>
2020-08-09 20:32:13 +0800

    Update Basics.js

--- Javascript/Basics.js
+++ Javascript/Basics.js
@@ -21,7 +21,6 @@ Boolean -> true || false
 //String Lengths and Retrieving the Number of Characters - variableName.length
 //Challenge: Changing Casing in Text -> variableName.toUpperCase()
 //Challenge: Changing String Casing Solution
-
 Basic Arithmetic and the Modulo Operator in Javascript
 Increment and Decrement Expressions
 Javascript Numbers Quiz
@@ -40,3 +39,42 @@ JavaScript Functions Quiz
 Tip from Angela - Set Your Expectations
 Feedback Time!
 
+  
+  // INTERMEDIATE JAVASCRIPT
+Random Number Generation in Javascript: Building a Love Calculator
+Control Statements: Using If-Else Conditionals & Logic
+Comparators and Equality
+Combining Comparators
+BMI Calculator Advanced (IF/ELSE)
+Introducing the Leap Year Code Challenge
+Leap Year Challenge Exercise
+Leap Year Solution
+Collections: Working with Javascript Arrays
+Adding Elements and Intermediate Array Techniques
+Who's Buying Lunch? Code Challenge
+Who's Buying Lunch Solution
+Control Statements: While Loops
+Solution to the 99 Bottles Challenge
+Control Statements: For Loops
+Introducing the Fibonacci Code Challenge
+The Fibonacci Exercise
+Fibonacci Solution
+Tip from Angela - Retrieval is How You Learn
+Feedback Time!
+
+  
+  //DOCUMENT OBJECT MODEL
+Adding Javascript to Websites
+Introduction to the Document Object Model (DOM)
+Solution to the DOM Challenge
+Selecting HTML Elements with Javascript
+Manipulating and Changing Styles of HTML Elements with Javascript
+The Separation of Concerns: Structure vs Style vs Behaviour
+Text Manipulation and the Text Content Property
+Manipulating HTML Element Attributes
+Tip from Angela - The 20 Minute Method
+Feedback Time!
+
+  
+  
+  
